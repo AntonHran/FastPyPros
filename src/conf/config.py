c9db4259308922cf97e1214fd9cf675d532dae45
Vitalyi <evciu97@gmail.com>
2023-10-14 18:34:37 +0300

    Update config.py

--- src/conf/config.py
+++ src/conf/config.py
@@ -3,24 +3,24 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_env: str = "dev"
-    postgres_db: str = "db_name"
-    postgres_user: str = "db_user"
-    postgres_password: str = "password"
-    postgres_port: str = "db_port"
-    sqlalchemy_database_url: str = 'postgresql+psycopg2://user:password@localhost:5432/postgres'
-    secret_key_a: str = "secret"
-    secret_key_r: str = "secret"
-    algorithm: str = "HS256"
-    mail_username: str = "example@meta.ua"
-    mail_password: str = "password"
-    mail_from: str = "example@meta.ua"
-    mail_port: int = 465
-    mail_server: str = "smtp.meta.ua"
-    redis_host: str = 'localhost'
-    redis_port: int = 6379
-    cloudinary_name: str = "name"
-    cloudinary_api_key: int = 00000000000
-    cloudinary_api_secret: str = "secret"
+    postgres_db: str 
+    postgres_user: str
+    postgres_password: str 
+    postgres_port: str 
+    sqlalchemy_database_url: str 
+    secret_key_a: str 
+    secret_key_r: str 
+    algorithm: str 
+    mail_username: str 
+    mail_password: str
+    mail_from: str 
+    mail_port: int 
+    mail_server: str 
+    redis_host: str 
+    redis_port: int 
+    cloudinary_name: str 
+    cloudinary_api_key: int 
+    cloudinary_api_secret: str 
 
     model_config = SettingsConfigDict(
         env_file=".env",
